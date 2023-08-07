cdfca70436f7044d913dc897b555cfc79f5f1596
Sergei Popinevskii <gurza000@gmail.com>
2023-08-07 14:15:30 +0300

    implement script to delete teams app

---
+++ uninstall-apps/delete_teams.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Path to Microsoft Teams classic app
+TEAMS_APP_PATH="/Applications/Microsoft Teams classic.app"
+
+if [ -d "$TEAMS_APP_PATH" ]; then
+    echo "Deleting Microsoft Teams classic..."
+    rm -rf "$TEAMS_APP_PATH"
+    echo "Microsoft Teams classic has been deleted."
+else
+    echo "Microsoft Teams classic is not installed."
+fi
