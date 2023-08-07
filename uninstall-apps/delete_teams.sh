6d8d07f367e419a09b0673e736aeb36d05a9a1fe
Sergei Popinevskii <gurza000@gmail.com>
2023-08-07 14:25:16 +0300

    delete related data

--- uninstall-apps/delete_teams.sh
+++ uninstall-apps/delete_teams.sh
@@ -2,6 +2,30 @@
 
 # Path to Microsoft Teams classic app
 TEAMS_APP_PATH="/Applications/Microsoft Teams classic.app"
+# Paths to Microsoft Teams related data
+TEAMS_DATA_PATHS=(
+    "$HOME/Library/Caches/com.microsoft.teams"
+    "$HOME/Library/Caches/com.microsoft.teams.shipit"
+    "$HOME/Library/Application Support/Microsoft/Teams"
+    "$HOME/Library/Application Support/Microsoft/Teams/Application Cache/Cache"
+    "$HOME/Library/Application Support/Microsoft/Teams/blob_storage"
+    "$HOME/Library/Application Support/Microsoft/Teams/Cache"
+    "$HOME/Library/Application Support/Microsoft/Teams/databases"
+    "$HOME/Library/Application Support/Microsoft/Teams/GPUCache"
+    "$HOME/Library/Application Support/Microsoft/Teams/IndexedDB"
+    "$HOME/Library/Application Support/Microsoft/Teams/Local Storage"
+    "$HOME/Library/Application Support/Microsoft/Teams/tmp"
+)
+
+delete_directory() {
+    if [ -d "$1" ]; then
+        echo "Deleting $1..."
+        rm -rf "$1"
+        echo "$1 has been deleted."
+    else
+        echo "$1 is not present."
+    fi
+}
 
 if [ -d "$TEAMS_APP_PATH" ]; then
     echo "Deleting Microsoft Teams classic..."
@@ -10,3 +34,8 @@ if [ -d "$TEAMS_APP_PATH" ]; then
 else
     echo "Microsoft Teams classic is not installed."
 fi
+
+# Delete Microsoft Teams related data
+for path in "${TEAMS_DATA_PATHS[@]}"; do
+    delete_directory "$path"
+done
